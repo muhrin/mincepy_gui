77767bc3375a5822b3ae1dd7013e4cd7527e1541
Martin Uhrin <martin.uhrin@gmail.com>
2020-03-08 22:20:46 +0100

    Setting up repository

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+__author__ = "Martin Uhrin and Sonia Collaud"
+__license__ = "GPLv3 and MIT, see LICENSE file"
+
+about = {}
+with open('mincepy_gui/version.py') as f:
+    exec(f.read(), about)
+
+setup(name='mincepy-gui',
+      version=about['__version__'],
+      description="Object storage with versioning made simple",
+      long_description=open('README.rst').read(),
+      url='https://github.com/muhrin/mincepy_gui.git',
+      author='Martin Uhrin',
+      author_email='martin.uhrin.10@ucl.ac.uk',
+      license=__license__,
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      keywords='database schemaless nosql object-store gui',
+      install_requires=[
+          'mincepy>=0.9.11',
+          'PySide2',
+      ],
+      extras_require={
+          'gui': [],
+          'dev': [
+              'pip',
+              'pytest>4',
+              'pytest-cov',
+              'pre-commit',
+              'yapf',
+              'prospector',
+              'pylint',
+              'twine',
+          ],
+      },
+      packages=['mincepy_gui'],
+      include_package_data=True,
+      test_suite='test',
+      provides=[],
+      entry_points={})
