3de5d3f4e2cefead672cc3f39031c45db4f8bc4d
Martin Uhrin <martin.uhrin@gmail.com>
2020-03-15 13:28:02 +0100

    Resolved QT::AA_ShareOpenGLContexts warning
    We aren't using the web browser plugin anyway, it's just being loaded by
    the UI loader so we get this warning.  To make it shut up I just set the
    attribute in start() and be done with it.

--- mincepy_gui/main.py
+++ mincepy_gui/main.py
@@ -5,7 +5,7 @@ import sys
 
 from PySide2.QtUiTools import QUiLoader
 from PySide2 import QtWidgets
-from PySide2.QtCore import QFile
+from PySide2.QtCore import QFile, Qt
 
 from . import main_controllers
 
@@ -15,6 +15,9 @@ RESOURCES = Path(__file__).parent / 'res'
 
 
 def start():
+    if hasattr(Qt, 'AA_ShareOpenGLContexts'):
+        QtWidgets.QApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
+
     app = QtWidgets.QApplication(sys.argv)
 
     ui_file = QFile(str(RESOURCES / "mainwindow.ui"))
